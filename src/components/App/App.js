60f96a2704b1e52cd8c9e529c8631a01f6c389ab
evalir.io <hi.evalir@gmail.com>
2019-05-09 18:17:20 -0400

    First commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function App() {
+  return (
+    <div>
+      klk
+      <div>con klk</div>
+    </div>
+  );
+}
+
+export default connect()(App);
